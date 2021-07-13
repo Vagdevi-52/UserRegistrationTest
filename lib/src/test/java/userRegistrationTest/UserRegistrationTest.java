4c48869de66e722fc9ef0b1421dae041e0e9cdfc
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-13 06:25:53 -0700

    Added: Program To Enter Valid First Name

--- lib/src/test/java/userRegistrationTest/UserRegistrationTest.java
+++ lib/src/test/java/userRegistrationTest/UserRegistrationTest.java
@@ -1,6 +1,5 @@
 package userRegistrationTest;
 
-
 import org.junit.Assert;
 import org.junit.Test;
 public class UserRegistrationTest
@@ -8,68 +7,35 @@ public class UserRegistrationTest
 
     UserRegistration userRegistration = new UserRegistration();
 
-    //test case for first name should start with upper case.
+    //Test case For first name when first letter start with upper case.
     @Test
      public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue()
-     {
-	   boolean result = userRegistration.validateFirstName("Vagdevi");
-	   Assert.assertTrue(result);
+    {
+	 boolean result = userRegistration.validateFirstName("Vagdevi");
+	 Assert.assertTrue(result);
 	}
     
-    //test case for first name which is less than three charcters.
+    //Test case for first name which is less tha three charcters.
      @Test
      public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse()
      {
 	   boolean result = userRegistration.validateFirstName("Va");
-	   Assert.assertFalse(result);  
+	   Assert.assertFalse(result);
     }
-     
+    
      //test case for first name which has a number.
      @Test
- 	 public void givenFirstName_WhenHasNumber_ShouldReturnFalse() 
+ 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse()
      {
  		boolean result = userRegistration.validateFirstName("Vagdevi52");
  		Assert.assertFalse(result);
- 	 }
+ 	}
 
-     //test case for the first name which has special character.
+     //Test case for first name which has a special character.
     @Test
     public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse()
     {
-	 boolean result = userRegistration.validateFirstName("V@gdevi");
-	 Assert.assertFalse(result);
-    }
-    
-    //Test case for the Last Name should start with uppercase.
-    @Test
-	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue()
-    {
-	    boolean result = userRegistration.validateLastName("Dhulipala");
-	    Assert.assertTrue(result);
-	}
-	
-    //test case for the LastName which has less than three charcters.
-	@Test 
-	public void givenLastName_WhenLessThanThreeChar_shouldReturnFalse() 
-	{
-		boolean result = userRegistration.validateLastName("De");
-		Assert.assertFalse(result);
-	}
-	
-	//test case for the LastName which has a number.
-	@Test
-	  public void givenLastName_WhenHasNumber_ShouldReturnFalse()
-	{
-	      boolean result = userRegistration.validateLastName("Vagdevi52");
-	      Assert.assertFalse(result);
-	  }
-
-	  //test case which has a special charcter.
-	  @Test
-	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse()
-	  {
-	      boolean result = userRegistration.validateLastName("V@gdevi");
-	      Assert.assertFalse(result);
-	  }
-
-}
+	  boolean result = userRegistration.validateFirstName("V@gdevi");
+	  Assert.assertFalse(result);
+   }
+}    
