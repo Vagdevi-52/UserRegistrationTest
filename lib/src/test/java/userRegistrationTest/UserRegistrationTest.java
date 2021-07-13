35363ac1f7ed782062e167858a7febdd83b34848
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-13 10:20:10 -0700

    Added: Program To Follow Pre-defined Password Rules

--- lib/src/test/java/userRegistrationTest/UserRegistrationTest.java
+++ lib/src/test/java/userRegistrationTest/UserRegistrationTest.java
@@ -1,109 +1,128 @@
 package userRegistrationTest;
 
-
 import org.junit.Assert;
 import org.junit.Test;
-public class UserRegistrationTest
-{
+public class UserRegistrationTest {
 
-    UserRegistration userRegistration = new UserRegistration();
-  
-     //Test For First name where it should with upper case.
+UserRegistration userRegistration = new UserRegistration();
+
     @Test
-     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue() 
-    {
-	  boolean result = userRegistration.validateFirstName("Vagdevi");
-	  Assert.assertTrue(result);
+     public void givenFirstName_WhenFirstLetterUpperCase_ShouldReturnTrue() {
+	 boolean result = userRegistration.validateFirstName("Vagdevi");
+	 Assert.assertTrue(result);
 	}
-    
-    //Test For first name when it has less than three charcters
      @Test
-     public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse()
-     {
-	   boolean result = userRegistration.validateFirstName("Va");
-	   Assert.assertFalse(result);
+     public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse() {
+	 boolean result = userRegistration.validateFirstName("Va");
+	Assert.assertFalse(result);
     }
-   
-     //first name when it has number
+
      @Test
- 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse() 
+ 	public void givenFirstName_WhenHasNumber_ShouldReturnFalse()
      {
  		boolean result = userRegistration.validateFirstName("Vagdevi52");
  		Assert.assertFalse(result);
  	}
 
-     //first name when it has a special charcter
     @Test
     public void givenFirstName_WhenHasSpecialChar_ShouldReturnFalse()
     {
-	 boolean result = userRegistration.validateFirstName("V@gdevi");
-	 Assert.assertFalse(result);
-   }
-    
-    //Test case for last name should start uppercase.
+	boolean result = userRegistration.validateFirstName("V@devi");
+	Assert.assertFalse(result);
+}
     @Test
-	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue()
-    {
+	public void givenLastName_WhenFirstLetterUpperCase_ShouldReturnTrue(){
 	    boolean result = userRegistration.validateLastName("Dhulipala");
 	    Assert.assertTrue(result);
 	}
 	
-    //Test case for last name it has less than three charcters.
 	@Test 
-	public void givenLastName_WhenLessThanThreeChar_shouldReturnFalse()
-	{
+	public void givenLastName_WhenLessThanThreeChar_shouldReturnFalse() {
 		boolean result = userRegistration.validateLastName("De");
 		Assert.assertFalse(result);
 	}
 	
-	//Test case for last name when it has number. 
 	@Test
-	  public void givenLastName_WhenHasNumber_ShouldReturnFalse() {
-	      boolean result = userRegistration.validateFirstName("Dhulipala72");
+	  public void givenLastName_WhenHasNumber_ShouldReturnFalse()
+	  {
+	      boolean result = userRegistration.validateFirstName("Dhulipala52");
 	      Assert.assertFalse(result);
 	  }
 
-	  //Test case for last name when it has special charcter
 	  @Test
-	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() {
-	      boolean result = userRegistration.validateFirstName("dhul@pala");
+	  public void givenLastName_WhenHasSpecialChar_ShouldReturnFalse() 
+	  {
+	      boolean result = userRegistration.validateFirstName("Dhul@pala");
 	      Assert.assertFalse(result);
 	  }
-	  //test case for emailid
 	  @Test
-	    public void givenEmailId_WhenProper_ShouldReturnTrue(){
+	    public void givenEmailId_WhenProper_ShouldReturnTrue()
+	    {
 	        boolean result = userRegistration.validateEmailid("abc.syz@bl.co.in");
 	        Assert.assertTrue(result);
 	    }
-	  
-	  //test case for the mobile number where the space is mentioned
 	  @Test
-	    public void givenMobileNum_WhenSpaceIsMentioned_ShouldReturnTrue()
-	  {
-	        boolean result = userRegistration.validateMobileNum("91 9959506738");
+	    public void givenMobileNum_WhenSpaceIsMentioned_ShouldReturnTrue(){
+	        boolean result = userRegistration.validateMobileNum("91 9993588735");
 	        Assert.assertTrue(result);
 	    }
-	  
-	    //test case for the mobile number where the space is mentioned
 	    @Test
 	    public void givenMobileNum_WhenSpaceNotMentioned_ShouldReturnFalse() {
-	        boolean result = userRegistration.validateMobileNum("9197894527");
-	        Assert.assertFalse(result);
-	    }
-        //test case for mobile number it has less digits.
-	    @Test
-	    public void givenMobileNum_WhenLessDigits_ShouldReturnFalse() 
-	    {
-	        boolean result = userRegistration.validateMobileNum("99674");
-	        Assert.assertFalse(result);
-	    }
-       //test case for mobile number it should not start with 91 code.
-	    @Test
-	    public void givenMobileNum_WhenNotStartWith91_ShouldReturnFalse()
-	    {
-	        boolean result = userRegistration.validateMobileNum("12345678");
+	        boolean result = userRegistration.validateMobileNum("919993588735");
 	        Assert.assertFalse(result);
 	    }
 
+	    @Test
+	    public void givenMobileNum_WhenLessDigits_ShouldReturnFalse() {
+	        boolean result = userRegistration.validateMobileNum("9967483421");
+	        Assert.assertFalse(result);
+	    }
 
-}
+	    @Test
+	    public void givenMobileNum_WhenNotStartWith91_ShouldReturnFalse() {
+	        boolean result = userRegistration.validateMobileNum("1245437234");
+	        Assert.assertFalse(result);
+	    }
+	    @Test
+		public void givenPassword_WhenMin8Chars_ShouldReturnTrue() {
+			boolean result = userRegistration.validatePassword("nDj1k2*jf");
+			Assert.assertTrue(result);
+		}
+
+		@Test
+		public void givenPassword_WhenAtleast1UpperCase_ShouldReturnTrue() {
+			boolean result = userRegistration.validatePassword("3Rmdg*nK");
+			Assert.assertTrue(result);
+		}
+
+		@Test
+		public void givenPassword_WhenAtleast1Number_ShouldReturnTrue() {
+			boolean result = userRegistration.validatePassword("Mas1bt&Bx");
+			Assert.assertTrue(result);
+		}
+
+		@Test
+		public void givenPassword_WhenNoNumeric_ShouldReturnFalse() {
+			boolean result = userRegistration.validatePassword("mDkk%fDB");
+			Assert.assertFalse(result);
+		}
+
+		@Test
+		public void givenPassword_WhenHasExact1SpecialChar_ShouldReturnTrue() {
+			boolean result = userRegistration.validatePassword("mDkk%fD1");
+			Assert.assertTrue(result);
+		}
+
+		@Test
+		public void givenPassword_WhenNoSpecialChar_ShouldReturnFlase() {
+			boolean result = userRegistration.validatePassword("mDkkfD12");
+			Assert.assertTrue(result);
+		}
+
+		@Test
+		public void givenPassword_WhenHasMoreThan1SpecialChar_ShouldReturnFalse() {
+			boolean result = userRegistration.validatePassword("mDkk%f#D1");
+			Assert.assertTrue(result);
+		}
+
+	}
