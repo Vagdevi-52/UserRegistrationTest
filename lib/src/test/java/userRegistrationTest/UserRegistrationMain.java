37035bc5a006a77e8d0a6593738cdaf1589861a3
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-14 10:17:40 -0700

    Added: Program For The Exceptions In Case Of Invalid Entry

---
+++ lib/src/test/java/userRegistrationTest/UserRegistrationMain.java
@@ -0,0 +1,15 @@
+package userRegistrationTest;
+
+class UserRegistrationMain
+{
+     public static void main(String[] args) 
+	 {
+		UserRegistration myObj = new UserRegistration();
+		myObj.firstNameValidate();
+		myObj.lastNameValidate();
+		myObj.emailValidate();
+		myObj.mobileValidate();
+		myObj.passwordValidate();
+	 }
+
+}
